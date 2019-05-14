3e94de565bc40be51554c994c2fa8d45776c3eb8
Aurora Shido-Wagenet <fullmetalwoman@gmail.com>
2019-05-14 13:57:07 -0700

    add customer images and random assignment

--- src/components/Window/Customer.jsx
+++ src/components/Window/Customer.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import customer1 from './../../assets/images/Customer.png';
+import customer2 from './../../assets/images/Customer2.png';
+import customer3 from './../../assets/images/Customer3.png';
 
 function Customer(){
+  const customers = [customer1, customer2, customer3];
   return(
-    <div>insert person here</div>
+    <div>
+      <img src={customers[(Math.floor(Math.random()*4)) - 1]} alt="customer"/>
+    </div>
   )
 }
 
