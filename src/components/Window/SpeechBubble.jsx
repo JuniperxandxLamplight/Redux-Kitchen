d321687cd0b5e93c346fe854dbc3d0eed594aede
Aurora Shido-Wagenet <fullmetalwoman@gmail.com>
2019-05-14 14:04:00 -0700

    add static speech bubble

--- src/components/Window/SpeechBubble.jsx
+++ src/components/Window/SpeechBubble.jsx
@@ -2,7 +2,20 @@ import React from 'react';
 
 function SpeechBubble(){
   return(
-    <div>GIMME WAFFLE</div>
+    <div>
+      <p>1 waffle please!</p>
+      <style jsx>{`
+        div{
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          width: 30%;
+          height: 150px;
+          border: 3px solid black;
+          border-radius: 100%;
+        }
+      `}</style>
+    </div>
   );
 }
 
