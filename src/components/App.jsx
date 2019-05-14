46ce7209c2d3c98838329d194bf17ea31cb171bd
wc <wchamberlain89@gmail.com>
2019-05-14 11:07:20 -0700

    Has working Header Component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,9 +5,16 @@ import GameContainer from './GameContainer';
 function App() {
   return (
     <div>
+      <style jsx global>{`
+        * {
+          margin : 0;
+          padding: 0;
+          box-sizing: border-box;
+        }
+        `}</style>
       <Header/>
-      <GameContainer />
     </div>
+
   );
 }
 
