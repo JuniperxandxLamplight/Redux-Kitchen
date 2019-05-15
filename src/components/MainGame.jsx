18814f3f93612a79bfe0ca8fb34f3d0903828bd6
Aurora Shido-Wagenet <fullmetalwoman@gmail.com>
2019-05-15 11:54:22 -0700

    add dynamic window for day/night

--- src/components/MainGame.jsx
+++ src/components/MainGame.jsx
@@ -20,6 +20,7 @@ function MainGame() {
           display: grid;
           grid-template-rows: repeat(3, 1fr);
           grid-template-columns: repeat(4, 1fr);
+          overflow: hidden;
         }
         .left{
           grid-row: 1/4;
