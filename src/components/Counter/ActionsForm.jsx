085942a1823e4cb7aeeb127421cd0fb581f9416a
Kimberly McConnell <kmcconnell393@gmail.com>
2019-05-15 23:42:42 -0700

    begin moving form to another component

--- src/components/Counter/ActionsForm.jsx
+++ src/components/Counter/ActionsForm.jsx
@@ -2,25 +2,23 @@ import React from 'react';
 import {connect} from 'react-redux';
 import { nextCustomer, userStateChange, dayToggle } from './../../actions';
 import constants from './../../constants';
+import ActionDayOne from './FormComponents/ActionDayOne';
 
 const { levels } = constants;
 
 
-function ActionsForm(props){
-  const form = <form onSubmit={handleActionSubmission}>
-        <p>action = {'{'}</p>
-        <p>&ensp; type : <input type='text' id='type' placeholder='action type' ref={(input) => {_type = input;}}/>
-        </p><p>{'}'}</p>
-        <button type='submit'>Dispatch</button>
-      </form>;
+class ActionsForm extends React.Component{
+  constructor(props){
+    super(props);
+    const userLevel = props.userLevel;
+    const customer = props.customerCount;
+    let _type = null;
+    this.handleActionSubmission = this.handleActionSubmission.bind(this);
+  }
+  
 
-  const forms = levels.action1;
 
-  const userLevel = props.userLevel;
-  const customer = props.customerCount;
-  let _type = null;
-
-  function handleActionSubmission(e) {
+  handleActionSubmission(e) {
     e.preventDefault();
     // check for correct answer
     if (_type.value === levels[userLevel].answersDay[(customer - 1)]){
@@ -41,51 +39,55 @@ function ActionsForm(props){
     _type.value = '';
   }
 
-  return(
-    <div className="container">
-      <div className='numbersBox'>
-        <p>1</p>
-        <p>2</p>
-        <p>3</p>
-        <p>4</p>
-        <p>5</p>
-      </div>
-      <form onSubmit={handleActionSubmission}>
-        <p>action = {'{'}</p>
-        <p>
-          type :
-          <input type='text' id='type' placeholder="action type" ref={(input) => {_type = input;}}/>
-        </p>
-        <p>{'}'}</p>
-        <button type="submit">Dispatch</button>
-      </form>
-      <style jsx>{`
-        .container{
-          display: flex;
-        }
-        input{
-          display: inline;
-        }
-        .numbersBox{
-          text-align: center;
-          padding: 5px;
-          background-color: #D1AF6E;
-        }
-        form{
-          padding: 5px 70px 5px 15px;
-          background-color: #C4C4C4;
-        }
-        button{
-          margin-left: 30%;
-          padding: 3px 15px;
-          border: none;
-          border-radius: 5px;
-          background-color: red;
-          cursor: pointer;
-        }
-      `}</style>
-    </div>
-  );
+  render(){
+
+    return(
+      <ActionDayOne onActionSubmission={this.handleActionSubmission}/>
+    //   <div className="container">
+    //     <div className='numbersBox'>
+    //       <p>1</p>
+    //       <p>2</p>
+    //       <p>3</p>
+    //       <p>4</p>
+    //       <p>5</p>
+    //     </div>
+    //     <form onSubmit={handleActionSubmission}>
+    //       <p>action = {'{'}</p>
+    //       <p>
+    //         type :
+    //         <input type='text' id='type' placeholder="action type" ref={(input) => {_type = input;}}/>
+    //       </p>
+    //       <p>{'}'}</p>
+    //       <button type="submit">Dispatch</button>
+    //     </form>
+    //     <style jsx>{`
+    //       .container{
+    //         display: flex;
+    //       }
+    //       input{
+    //         display: inline;
+    //       }
+    //       .numbersBox{
+    //         text-align: center;
+    //         padding: 5px;
+    //         background-color: #D1AF6E;
+    //       }
+    //       form{
+    //         padding: 5px 70px 5px 15px;
+    //         background-color: #C4C4C4;
+    //       }
+    //       button{
+    //         margin-left: 30%;
+    //         padding: 3px 15px;
+    //         border: none;
+    //         border-radius: 5px;
+    //         background-color: red;
+    //         cursor: pointer;
+    //       }
+    //     `}</style>
+    //   </div>
+    );
+  }
 }
 
 const mapStateToProps = state => {
--- src/components/Counter/FormComponents/ActionDayOne.jsx
+++ src/components/Counter/FormComponents/ActionDayOne.jsx
@@ -1,9 +1,52 @@
 import React from 'react';
 
-function ActionDayOne() {
+function ActionDayOne(props) {
+  let _type = null;
   return(
-    <div></div>
-  )
+    <div className="container">
+        <div className='numbersBox'>
+          <p>1</p>
+          <p>2</p>
+          <p>3</p>
+          <p>4</p>
+          <p>5</p>
+        </div>
+        <form onSubmit={props.onActionSubmission}>
+          <p>action = {'{'}</p>
+          <p>
+            type :
+            <input type='text' id='type' placeholder="action type" ref={(input) => {_type = input;}}/>
+          </p>
+          <p>{'}'}</p>
+          <button type="submit">Dispatch</button>
+        </form>
+        <style jsx>{`
+          .container{
+            display: flex;
+          }
+          input{
+            display: inline;
+          }
+          .numbersBox{
+            text-align: center;
+            padding: 5px;
+            background-color: #D1AF6E;
+          }
+          form{
+            padding: 5px 70px 5px 15px;
+            background-color: #C4C4C4;
+          }
+          button{
+            margin-left: 30%;
+            padding: 3px 15px;
+            border: none;
+            border-radius: 5px;
+            background-color: red;
+            cursor: pointer;
+          }
+        `}</style>
+      </div>
+    );
 }
 
 export default ActionDayOne;
