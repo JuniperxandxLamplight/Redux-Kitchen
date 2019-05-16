2884c6d02f1fdf61144259076e6460e94218a7a9
wc <wchamberlain89@gmail.com>
2019-05-16 09:16:45 -0700

    It will proceed through screens

--- src/components/Counter/ActionsForm.jsx
+++ src/components/Counter/ActionsForm.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import {connect} from 'react-redux';
 import { nextCustomer, userStateChange, dayToggle } from './../../actions';
 import constants from './../../constants';
 const {levels} = constants;
@@ -9,30 +8,23 @@ import Line from './Line';
 import ReactHtmlParser from 'react-html-parser';
 
 class ActionsForm extends React.Component {
-  constructor(props){
-    super(props);
-    this.userLevel = props.userLevel;
-    this.customer = props.customerCount;
-    this._type = null;
-  }
-
-
   handleActionSubmission(e) {
     e.preventDefault();
     console.log("here");
     // check for correct answer
     console.log(this.refs);
+    console.log(this.props.customerCount)
 
-    if (this.refs.input1.value === levels[this.userLevel].answersDay[(this.customer - 1)]){
-      // check for this.customer count
-      if (this.customer >= 3) {
+    if (this.refs.input1.value === levels[this.props.userLevel].answersDay[(this.props.customerCount - 1)]){
+      // check for this.props.customer count
+      if (this.props.customerCount >= 3) {
         // toggle to night and reset cutomercount to 1
         this.props.dispatch(dayToggle());
-        this.props.dispatch(userStateChange(levels[this.userLevel].answersDay[(this.customer - 1)]))
+        this.props.dispatch(userStateChange(levels[this.props.userLevel].answersDay[(this.props.customerCount - 1)]))
       } else {
         // go to the next customer
         this.props.dispatch(nextCustomer());
-        this.props.dispatch(userStateChange(levels[this.userLevel].answersDay[(this.customer - 1)]))
+        this.props.dispatch(userStateChange(levels[this.props.userLevel].answersDay[(this.props.customerCount - 1)]))
       }
     } else {
       // Do some animation
@@ -46,7 +38,7 @@ render(){
     <div className="container">
       <form onSubmit={this.handleActionSubmission.bind(this)}>
         {
-          levels[this.userLevel].promptDay.map(function(lineText, index){
+          levels[this.props.userLevel].promptDay.map(function(lineText, index){
             return <div key={index}>{ReactHtmlParser(lineText)}</div>
           })
         }
