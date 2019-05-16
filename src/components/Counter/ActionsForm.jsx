c98da64b77c8c9846e948ed18560b667f5a270ad
Kimberly McConnell <kmcconnell393@gmail.com>
2019-05-16 08:24:37 -0700

    make form-related dispatched work

--- src/components/Counter/ActionsForm.jsx
+++ src/components/Counter/ActionsForm.jsx
@@ -7,30 +7,32 @@ import ActionDayOne from './FormComponents/ActionDayOne';
 const { levels } = constants;
 
 
+
 class ActionsForm extends React.Component{
   constructor(props){
     super(props);
     this.userLevel = props.userLevel;
-    this.customer = props.customerCount;
+    this.customerCount = props.customerCount;
+    this.dayTime = props.dayTime;
     this.handleActionSubmission = this.handleActionSubmission.bind(this);
-    
   }
   
 
 
   handleActionSubmission(userAnswer) {
+    console.log("cutomer#", this.props.customerCount)
     
     // check for correct answer
-    if (userAnswer === levels[this.userLevel].answersDay[(this.customer - 1)]){
+    if (userAnswer === levels[this.props.userLevel].answersDay[(this.props.customerCount - 1)]){
       // check for customer count
-      if (this.customer >= 3) {
+      if (this.props.customerCount >= 3) {
         // toggle to night and reset cutomercount to 1
         this.props.dispatch(dayToggle());
-        this.props.dispatch(userStateChange(levels[this.userLevel].answersDay[(this.customer - 1)]))
+        this.props.dispatch(userStateChange(levels[this.userLevel].answersDay[(this.props.customerCount - 1)]))
       } else {
         // go to the next customer
         this.props.dispatch(nextCustomer());
-        this.props.dispatch(userStateChange(levels[this.userLevel].answersDay[(this.customer - 1)]))
+        this.props.dispatch(userStateChange(levels[this.userLevel].answersDay[(this.props.customerCount - 1)]))
       }
     } else {
       // Do some animation
@@ -39,52 +41,18 @@ class ActionsForm extends React.Component{
   }
 
   render(){
-
+    console.log(this.props.dayTime);
+    let form;
+    if (this.props.dayTime) {
+      form = <ActionDayOne onActionSubmission={this.handleActionSubmission}/>
+    } else {
+      form = "";
+    }
+   
     return(
-      <ActionDayOne onActionSubmission={this.handleActionSubmission}/>
-    //   <div className="container">
-    //     <div className='numbersBox'>
-    //       <p>1</p>
-    //       <p>2</p>
-    //       <p>3</p>
-    //       <p>4</p>
-    //       <p>5</p>
-    //     </div>
-    //     <form onSubmit={handleActionSubmission}>
-    //       <p>action = {'{'}</p>
-    //       <p>
-    //         type :
-    //         <input type='text' id='type' placeholder="action type" ref={(input) => {_type = input;}}/>
-    //       </p>
-    //       <p>{'}'}</p>
-    //       <button type="submit">Dispatch</button>
-    //     </form>
-    //     <style jsx>{`
-    //       .container{
-    //         display: flex;
-    //       }
-    //       input{
-    //         display: inline;
-    //       }
-    //       .numbersBox{
-    //         text-align: center;
-    //         padding: 5px;
-    //         background-color: #D1AF6E;
-    //       }
-    //       form{
-    //         padding: 5px 70px 5px 15px;
-    //         background-color: #C4C4C4;
-    //       }
-    //       button{
-    //         margin-left: 30%;
-    //         padding: 3px 15px;
-    //         border: none;
-    //         border-radius: 5px;
-    //         background-color: red;
-    //         cursor: pointer;
-    //       }
-    //     `}</style>
-    //   </div>
+      <div>
+        {form}
+      </div>
     );
   }
 }
@@ -94,6 +62,7 @@ const mapStateToProps = state => {
   return {
     userLevel: state.userLevel,
     customerCount: state.customerCount,
+    dayTime: state.dayTime,
   }
 }
 
